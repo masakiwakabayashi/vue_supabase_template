b3e0a9d9036e97783c60c8ba7d554f3fed27a266
masakiwakabayashi <masaki.wakabaya@gmail.com>
2025-03-20 20:43:02 +0900

    update home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { supabase } from '@/lib/supabaseClient';
+
+const userEmail = ref('');
+
+onMounted(async () => {
+  const { data: { user } } = await supabase.auth.getUser();
+  if (user) {
+    userEmail.value = user.email;
+  }
+});
+</script>
+
 <template>
   <div class="bg-blue-50 min-h-screen p-6">
-    <div>
-      ホーム
+    <div v-if="userEmail" class="mt-4 text-gray-700">
+      ログイン中のユーザー: {{ userEmail }}
     </div>
   </div>
 </template>
